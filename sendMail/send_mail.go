f1a9c7b0898d4e077a927d4351cdb03ac994bf2e
tiar <tiar.agisti@gmail.com>
2024-05-18 17:41:13 +0700

    learn send email

--- sendMail/send_mail.go
+++ sendMail/send_mail.go
@@ -5,6 +5,7 @@ import (
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 	"log"
+	"os"
 )
 
 func main() {
@@ -14,7 +15,7 @@ func main() {
 	plainTextContent := "and easy to do anywhere, even with Go"
 	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-	client := sendgrid.NewSendClient("")
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	response, err := client.Send(message)
 	if err != nil {
 		log.Println(err)
